d2c6a3d52ac7f41e527234b64c35f79e0f297533
MargelovN <margelovn@gmail.com>
2021-09-17 20:01:25 +0900

    ADDED Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ruble = 20;
+        int ticketPrice = 42_451;
+        int mile = ticketPrice / ruble;
+        System.out.println("Миля : " + mile);
+    }
+}
